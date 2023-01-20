6d8bbc02f4b6630b7773b812bde4fd189a44ee95
HenriqueMVSS <henrique_mateus1994@hotmail.com>
2023-01-20 00:33:06 -0300

    adding localStorage

--- scripts.js
+++ scripts.js
@@ -1,11 +1,27 @@
-const form = document.querySelector('#form-habits');
-const nlwSetup = new NLWSetup(form)
+const form = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-  run: ['01-01', '01-02', '01-06'],
-  water: ['01-04', '01-05'],
-  academy: ['01-01', '01-03'],
+button.addEventListener("click", add);
+form.addEventListener("change", save);
+
+function add() {
+  let today = new Date().toLocaleDateString("pt-BR").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (!dayExists) {
+    nlwSetup.addDay(today);
+    alert("Dia cadastrado com sucesso 🟢");
+  } else {
+    alert("Dia já cadastrado 🛑");
+  }
 }
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
 
 nlwSetup.setData(data);
 nlwSetup.load();
